7cac93c6fec2ffaf7f20b448898cd178b6573a63
thatsoheeron <soheeron@gmail.com>
2026-04-19 15:22:04 -0400

    Customers will now leave after 60 seconds and it affects rating

--- Scripts/cauldron.gd
+++ Scripts/cauldron.gd
@@ -9,81 +9,83 @@ func _process(_delta: float) -> void:
 	if $Mute_Button.button_pressed == false && $Level_Background.playing == false:
 		$Level_Background.playing = true
 
+func _switch_timeline(_new_timeline_path: String):
+	Dialogic.start(_new_timeline_path)
 
 func _on_cauldron_fill_perfect() -> void:
 	if Dialogic.VAR.Current_Customer == "Adventurer":
-		Dialogic.start("res://Dialogic/Adventurer/Adv_Perfect.dtl")
+		_switch_timeline("res://Dialogic/Adventurer/Adv_Perfect.dtl")
 	elif Dialogic.VAR.Current_Customer == "Assassin":
-		Dialogic.start("res://Dialogic/Assassin/Ass_Perfect.dtl")
+		_switch_timeline("res://Dialogic/Assassin/Ass_Perfect.dtl")
 	elif  Dialogic.VAR.Current_Customer == "Guard":
-		Dialogic.start("res://Dialogic/Guard/Gua_Perfect.dtl")
+		_switch_timeline("res://Dialogic/Guard/Gua_Perfect.dtl")
 	elif  Dialogic.VAR.Current_Customer == "Nobleman":
-		Dialogic.start("res://Dialogic/Nobleman/Nob_Perfect.dtl")
+		_switch_timeline("res://Dialogic/Nobleman/Nob_Perfect.dtl")
 	else:
 		pass
 
 func _on_cauldron_fill_pretty_good() -> void:
 	if Dialogic.VAR.Current_Customer == "Adventurer":
-		Dialogic.start("res://Dialogic/Adventurer/Adv_Good.dtl")
+		_switch_timeline("res://Dialogic/Adventurer/Adv_Good.dtl")
 	elif Dialogic.VAR.Current_Customer == "Assassin":
-		Dialogic.start("res://Dialogic/Assassin/Ass_Good.dtl")
+		_switch_timeline("res://Dialogic/Assassin/Ass_Good.dtl")
 	elif Dialogic.VAR.Current_Customer == "Guard":
-		Dialogic.start("res://Dialogic/Guard/Gua_Good.dtl")
+		_switch_timeline("res://Dialogic/Guard/Gua_Good.dtl")
 	elif Dialogic.VAR.Current_Customer == "Nobleman":
-		Dialogic.start("res://Dialogic/Nobleman/Nob_Good.dtl")
+		_switch_timeline("res://Dialogic/Nobleman/Nob_Good.dtl")
 	else:
 		pass
 
 func _on_cauldron_fill_okay() -> void:
 	if Dialogic.VAR.Current_Customer == "Adventurer":
-		Dialogic.start("res://Dialogic/Adventurer/Adv_Okay.dtl")
+		_switch_timeline("res://Dialogic/Adventurer/Adv_Okay.dtl")
 	elif Dialogic.VAR.Current_Customer == "Assassin":
-		Dialogic.start("res://Dialogic/Assassin/Ass_Okay.dtl")
+		_switch_timeline("res://Dialogic/Assassin/Ass_Okay.dtl")
 	elif Dialogic.VAR.Current_Customer == "Guard":
-		Dialogic.start("res://Dialogic/Guard/Gua_Okay.dtl")
+		_switch_timeline("res://Dialogic/Guard/Gua_Okay.dtl")
 	elif Dialogic.VAR.Current_Customer == "Nobleman":
-		Dialogic.start("res://Dialogic/Nobleman/Nob_Okay.dtl")
+		_switch_timeline("res://Dialogic/Nobleman/Nob_Okay.dtl")
 	else:
 		pass
 
 
 func _on_cauldron_fill_meh() -> void:
 	if Dialogic.VAR.Current_Customer == "Adventurer":
-		Dialogic.start("res://Dialogic/Adventurer/Adv_Meh.dtl")
+		_switch_timeline("res://Dialogic/Adventurer/Adv_Meh.dtl")
 	elif Dialogic.VAR.Current_Customer == "Assassin":
-		Dialogic.start("res://Dialogic/Assassin/Ass_Meh.dtl")
+		_switch_timeline("res://Dialogic/Assassin/Ass_Meh.dtl")
 	elif Dialogic.VAR.Current_Customer == "Guard":
-		Dialogic.start("res://Dialogic/Guard/Gua_Meh.dtl")
+		_switch_timeline("res://Dialogic/Guard/Gua_Meh.dtl")
 	elif Dialogic.VAR.Current_Customer == "Nobleman":
-		Dialogic.start("res://Dialogic/Nobleman/Nob_Meh.dtl")
+		_switch_timeline("res://Dialogic/Nobleman/Nob_Meh.dtl")
 	else:
 		pass
 
 
 func _on_cauldron_fill_horrible() -> void:
 	if Dialogic.VAR.Current_Customer == "Adventurer":
-		Dialogic.start("res://Dialogic/Adventurer/Adv_Horrible.dtl")
+		_switch_timeline("res://Dialogic/Adventurer/Adv_Horrible.dtl")
 	elif Dialogic.VAR.Current_Customer == "Assassin":
-		Dialogic.start("res://Dialogic/Assassin/Ass_Horrible.dtl")
+		_switch_timeline("res://Dialogic/Assassin/Ass_Horrible.dtl")
 	elif Dialogic.VAR.Current_Customer == "Guard":
-		Dialogic.start("res://Dialogic/Guard/Gua_Horrible.dtl")
+		_switch_timeline("res://Dialogic/Guard/Gua_Horrible.dtl")
 	elif Dialogic.VAR.Current_Customer == "Nobleman":
-		Dialogic.start("res://Dialogic/Nobleman/Nob_Horrible.dtl")
+		_switch_timeline("res://Dialogic/Nobleman/Nob_Horrible.dtl")
 	else:
 		pass
-
 
 func _on_cauldron_fill_wrong_potion() -> void:
 	if Dialogic.VAR.Current_Customer == "Adventurer":
-		Dialogic.start("res://Dialogic/Adventurer/Adv_Wrong.dtl")
+		_switch_timeline("res://Dialogic/Adventurer/Adv_Wrong.dtl")
 	elif Dialogic.VAR.Current_Customer == "Assassin":
-		Dialogic.start("res://Dialogic/Assassin/Ass_Wrong.dtl")
+		_switch_timeline("res://Dialogic/Assassin/Ass_Wrong.dtl")
 	elif Dialogic.VAR.Current_Customer == "Guard":
-		Dialogic.start("res://Dialogic/Guard/Gua_Wrong.dtl")
+		_switch_timeline("res://Dialogic/Guard/Gua_Wrong.dtl")
 	elif Dialogic.VAR.Current_Customer == "Nobleman":
-		Dialogic.start("res://Dialogic/Nobleman/Nob_Wrong.dtl")
+		_switch_timeline("res://Dialogic/Nobleman/Nob_Wrong.dtl")
 	else:
 		pass
+
 
 func _load_main_menu() -> void:
 	Dialogic.clear()
